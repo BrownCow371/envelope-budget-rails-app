ab55b0a926280bb4f7a0fe2dc6fc462807a34836
Jamie Brown <jmbrowncow@gmail.com>
2018-09-07 21:43:14 +0000

    update varaible name in create action in users controller. add show view for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
-      session[:user_id]= user.id
+      session[:user_id]= @user.id
       redirect_to @user
     else
       render 'new'
