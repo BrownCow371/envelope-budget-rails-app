9496812fdc55ddd9ba912babcbd85850bf7a2e59
Jamie Brown <jmbrowncow@gmail.com>
2018-09-07 21:25:40 +0000

    add uniqueness to user model validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, presence: true
+  validates :username, uniqueness: true
+
   validates :email, presence: true
   validates :email, uniqueness: true
 
