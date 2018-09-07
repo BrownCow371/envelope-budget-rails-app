9c5599fe440dba58ef003753c2e13cb95ac0df21
Jamie Brown <jmbrowncow@gmail.com>
2018-09-07 18:44:48 +0000

    add scope methods to expenses model. update seeds to include test date for scopes

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -18,4 +18,36 @@ class Expense < ApplicationRecord
        !(Date.valid_date?(y.to_i, m.to_i, d.to_i))
      end
   end
+
+  def self.by_user(user_id)
+    where(user: user_id)
+  end
+
+  def self.by_category(category_id)
+    where(category: category_id)
+  end
+
+  def self.last_five
+    self.all.order("id DESC").limit(5)
+  end
+
+  # def self.largest_by_user(user_id)
+  #   where(user: user_id).order("exp_amount DESC").limit(1)
+  # end
+
+  def self.largest
+    self.order("exp_amount DESC").limit(1)
+  end
+
+  def self.from_today
+    where("exp_date =?", Date.today)
+  end
+
+  def self.from_this_month
+    month_number = Date.today.month
+    month_beginning = Date.new(Date.today.year, month_number)
+    month_ending = month_beginning.end_of_month
+    where("exp_date <= ? AND exp_date >= ?", month_ending, month_beginning)
+  end
+
 end
