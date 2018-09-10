9ba852e3aec2c64890c0593c603b1df617d1c087
Jamie Brown <jmbrowncow@gmail.com>
2018-09-10 15:07:30 +0000

    remove raise params in expense controller. add table partial for expenses and index view logic

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,6 @@ class ExpensesController < ApplicationController
   before_action :require_logged_in
 
   def index
-    # raise current_user.id.inspect
     @user = User.find_by_id(params[:user_id]) if params[:user_id]
 
     if @user && @user.id == current_user.id
