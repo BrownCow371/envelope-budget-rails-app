e20e927e3b72393239a09898a7ce6531dae3abf1
Jamie Brown <jmbrowncow@gmail.com>
2018-09-10 20:31:49 +0000

    add View Expense to table helpers in ExpenseHelpers. add cateory_selection helper method

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -6,6 +6,7 @@ module ExpensesHelper
               "Payee",
               "Amount",
               "Category",
+              "Expense Details",
               "Edit Expense",
               "Remove Expense"
               ]
@@ -18,9 +19,18 @@ module ExpensesHelper
               expense.payee,
               number_to_currency(expense.exp_amount),
               expense.category.name,
+              (link_to "View Expense", user_expense_path(user, expense)),
               (link_to "Edit Expense", edit_user_expense_path(user, expense)),
               (link_to "Remove Expense", user_expense_path(user, expense), method: :delete, data: { confirm: "Are you sure you want to remove this expense?" })
             ]
     data.collect { |column| content_tag(:td, column) }.join().html_safe
+  end
+
+  def category_selection(f, expense)
+    if expense.category
+      f.select :category_name, options_from_collection_for_select(Category.all, "name", "name", :selected=>expense.category.name), include_blank: true
+    else
+      f.select :category_name, options_from_collection_for_select(Category.all, "name", "name"), include_blank: true
+    end
   end
 end
