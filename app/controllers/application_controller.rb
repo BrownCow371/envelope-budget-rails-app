b59593c48752508704788083f0f09b5b8b06f1ae
Jamie Brown <jmbrowncow@gmail.com>
2018-09-10 14:29:41 +0000

    add logged_in?, current_user and require_logged_in helpers to Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    redirect_to root_path unless logged_in?
+  end
+
+  def current_user
+    @current_user ||=User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
