0bc3d9de7400cc28aeae8b9290586dd8c6edf6d7
Jamie Brown <jmbrowncow@gmail.com>
2018-09-10 22:29:00 +0000

    update order of before_actions for users controller so require_logged_in happens first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_logged_in, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def new
     @user=User.new
