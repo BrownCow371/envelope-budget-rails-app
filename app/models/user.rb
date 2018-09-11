7a33d5a5c22d1e232f77c7f5e5f246415c4afff5
Jamie Brown <jmbrowncow@gmail.com>
2018-09-11 02:27:40 +0000

    remove username validations from user model since can't get username from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :username, presence: true
-  validates :username, uniqueness: true
+  # validates :username, presence: true
+  # validates :username, uniqueness: true
 
   validates :email, presence: true
   validates :email, uniqueness: true
