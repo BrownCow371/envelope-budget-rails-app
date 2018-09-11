4876443034fb99b912af9a9edf7d73c559552581
Jamie Brown <jmbrowncow@gmail.com>
2018-09-11 14:32:31 +0000

    update expensescontroller actions to be skinny. remove duplicate logic

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,47 +3,25 @@ class ExpensesController < ApplicationController
   before_action :set_expense, only: [:show, :edit, :update, :destroy]
   before_action :set_user_by_params
 
-  def index
-    if user_valid?
-      if !params[:category].blank?
-        @expenses = @user.expenses.by_category(params[:category])
-        # flash[:message] = "#{Category.find_by_id(params[:category].to_i).name} Category Expenses"
-        flash[:message] = "#{@expenses.first.category.name} Category Expenses"
+  def stats
+  end
 
-      elsif !params[:date].blank?
-        if params[:date] == "Today"
-          @expenses = @user.expenses.from_today
-          flash[:message] = "Today's Expenses"
-        elsif params[:date] =="This Month"
-          @expenses = @user.expenses.from_this_month
-          flash[:message] = "This Month's Expenses"
-        end
-      else
-        @expenses = @user.expenses
-        flash.clear
-      end
-    end
+  def index
+    filter_results
   end
 
   def show
-    if !user_valid?
-      redirect_to user_expenses_path(current_user), alert: "User not found or you are not authorized to see this page, redirected to your expenses page."
-    elsif !@expense || @expense.user_id != current_user.id
-      redirect_to user_expenses_path(current_user), alert: "Expense not found or you are not authorized to see this page, redirected to your expenses page."
+    if !expense_user_valid?
+      redirect_to user_expenses_path(current_user), alert: "SHOW: Expense not found or you are not authorized to see this page, redirected to your expenses page."
     end
   end
 
   def new
     @expense = Expense.new
-    if !user_valid?
-      redirect_to new_user_expense_path(current_user), alert: "You can only add expenses to your own account. Redirected to your new expense page."
-    end
   end
 
   def create
-    # raise params.inspect
     @expense = @user.expenses.build(expense_params)
-
     if user_valid? && @expense.save
       redirect_to user_expenses_path(@user), alert: "Expense added!"
     else
@@ -52,35 +30,30 @@ class ExpensesController < ApplicationController
   end
 
   def edit
-    if !user_valid?
-      redirect_to user_expenses_path(current_user), alert: "User not found or you are not authorized to edit this expense, redirected to your expenses page."
-    elsif !@expense || @expense.user_id != current_user.id
-      redirect_to user_expenses_path(current_user), alert: "Expense not found or you are not authorized to edit this expense, redirected to your expenses page."
+    if !expense_user_valid?
+      redirect_to user_expenses_path(current_user), alert: "EDIT: Expense not found or you are not authorized to edit this expense, redirected to your expenses page."
     end
-
   end
 
   def update
-    # raise params.inspect
-    if user_valid? && @expense.user_id == @user.id
-      if @expense.update(expense_params)
-        redirect_to user_expenses_path(@user)
-      else
+    if expense_user_valid? && @expense.update(expense_params)
+        redirect_to user_expenses_path(@user), alert: "Expense Updated!"
+    elsif !@expense.update(expense_params)
         render 'edit'
-      end
     else
-      redirect_to user_expenses_path(current_user), alert: "Expense not found or you are not authorized to update this expense, redirected to your expenses page."
+      redirect_to user_expenses_path(current_user), alert: "UPDATE: Expense not found or you are not authorized to update this expense, redirected to your expenses page."
     end
   end
 
   def destroy
-    if user_valid? && @expense.user_id == @user.id
+    if user_valid? && expense_user_valid?
       @expense.destroy
       redirect_to user_expenses_path(@user), alert: "Expense removed from your list of expenses."
     else
-      redirect_to user_expenses_path(current_user), alert: "You cannot destroy expenses that are not assocaited with your account. Redirected to your expenses page."
+      redirect_to user_expenses_path(current_user), alert: "DELETE: You cannot destroy expenses that are not assocaited with your account. Redirected to your expenses page."
     end
   end
+
 
 private
 
@@ -91,7 +64,7 @@ private
   def set_user_by_params
     @user = User.find_by_id(params[:user_id]) if (params[:user_id] && current_user.id == params[:user_id].to_i)
     if @user.nil?
-      redirect_to user_path(current_user), alert: "User not found or you are not allowed to view the accounts of other users. Redirected to your account."
+      redirect_to user_path(current_user), alert: "SET USER ERROR: You cannot view or modify another user's account/expenses. Redirected to your account."
     end
   end
 
@@ -99,9 +72,39 @@ private
     @user && @user.id == current_user.id
   end
 
+  def expense_user_valid?
+    @expense && @expense.user_id == current_user.id
+  end
+
   def expense_params
     params.require(:expense).permit(:payee, :exp_date, :exp_amount, :category_name, category_new: [:name])
   end
 
+ def filter_results
+   if !params[:category].blank?
+     @expenses = @user.expenses.by_category(params[:category])
+     # flash[:message] = "#{Category.find_by_id(params[:category].to_i).name} Category Expenses"
+     flash[:message] = "#{@expenses.first.category.name} Category Expenses"
+   elsif !params[:date].blank?
+     if params[:date] == "Today"
+       @expenses = @user.expenses.from_today
+       flash[:message] = "Today's Expenses"
+     elsif params[:date] =="This Month"
+       @expenses = @user.expenses.from_this_month
+       flash[:message] = "This Month's Expenses"
+     end
+   else
+     @expenses = @user.expenses
+    flash[:message] = ""
+   end
+ end
+ 
+  # coming soon
+  # def stats_hash
+  #   largest expense
+  #   largest category
+  #   total today
+  #   total this month
+  # end
 
 end
