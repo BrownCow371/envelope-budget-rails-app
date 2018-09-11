6b39519b57de0cb8d462b86bcf44fe54596ab4a0
Jamie Brown <jmbrowncow@gmail.com>
2018-09-11 14:29:18 +0000

    remove unused code from category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,14 +4,4 @@ class Category < ApplicationRecord
 
   validates :name, presence: true
 
-  # def self.most_expenses
-  #   hash = self.joins(:users).group("category_id").sum("exp_amount")
-  #   # hash.select {|cat, sum| sum.max}
-  # end
-
-
-  # def self.sum_by_category(category_id)
-  #   where("category_id =?", category_id).group("category_id").sum("exp_amount").values[0]
-  # end
-
 end
