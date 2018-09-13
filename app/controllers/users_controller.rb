668cd1d509269993faae13edc2c9f3a15d886eb7
Jamie Brown <jmbrowncow@gmail.com>
2018-09-13 12:58:08 +0000

    add spaces to methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def new
-    @user=User.new
+    @user = User.new
   end
 
   def create
-    @user=User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       session[:user_id]= @user.id
       redirect_to @user
