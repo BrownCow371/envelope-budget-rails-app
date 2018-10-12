cf3bede1aa4a1e932759b255a93e395a83c66346
Jamie Brown <jmbrowncow@gmail.com>
2018-10-12 00:49:15 +0000

    modify routes for expenses and category show

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-
   resources :users do
     get '/expenses/stats' => 'expenses#stats', as: 'expenses_stats'
     get '/expenses/last_five' => 'expenses#last_five', as: 'expenses_last_five'
     get '/expenses/:id/jsontest' => 'expenses#jsontest'
     resources :expenses
-    resources :categories, only: [:show]
+    # resources :categories, only: [:show]
   end
 
-  resources :expenses
+  resources :categories, only: [:show]
+  # resources :expenses
 
   get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
