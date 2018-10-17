5e8fb7af4887f07c05a5e0a1ec56ea00d5db643f
Jamie Brown <jmbrowncow@gmail.com>
2018-10-17 20:06:21 +0000

    update user constructor function to a class

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,8 +1,10 @@
-function User(attributes){
+class User {
+  constructor(attributes){
   this.id = attributes.id;
   this.email = attributes.email;
   // this.username = attributes.username;
   this.name = attributes.name;
   this.expenses = attributes.expenses;
   this.categories = attributes.categories;
+  }
 }
