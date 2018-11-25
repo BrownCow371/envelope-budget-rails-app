0845af094396d4c6faf0b9ff790a80c3a1876d85
BrownCow371 <jmbrowncow@gmail.com>
2018-11-25 15:43:14 -0800

    remove //= require jquery_ujs from manifest - duplicates alert confirmation boxes. just need jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+
 //= require rails-ujs
 //= require activestorage
 //= require expenses
@@ -21,4 +21,4 @@
 //= require listeners
 
 
-// = require_tree .
+
